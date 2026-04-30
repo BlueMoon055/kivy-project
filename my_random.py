f58fe1b0ae5cc16da2f11de27a030dc2a5b52281
Daniil <danilabajkalov@gmail.com>
2026-04-30 18:54:06 +0300

    code

--- my_random.py
+++ my_random.py
@@ -7,26 +7,39 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.core.window import Window
 
+
 Window.size = (300, 690)
 Window.title = 'my Random'
+Window.clearcolor = (1, 1, 1, 1)
 
 class MenuScreen(Screen):  # класс-наследник
     def __init__(self, **kw):  # конструктор
         super(MenuScreen, self).__init__(**kw)  # наследование свойств из супер-класса
         self.box = BoxLayout(orientation='vertical')
-        self.main_text = Label(text='my RANDOM')
-        self.image_food = Image(source='food.jpg')
-        self.food_text = Label(text='Блюдо')
-        self.image_sound = Image(source='sound.jpeg')
-        self.sound_text = Label(text='Случайный звук')
-        self.image_cube = Image(source='кубик.jpg')
-        self.cube_text = Label(text='Бросить кубик')
+
+        # Указываем, что Label будет занимать всю ширину
+        self.main_text = Label(
+            text='my RANDOM',
+            halign="left",
+            color=(0, 0, 0, 1),
+            size_hint=(1, None),  # растягиваем по ширине
+            height=50,)  # фиксированная высота
+
+        self.main_text.bind(size=self.main_text.setter('text_size'))  # Устанавливаем text_size принудительно
+        # Растягиваем картинку на всю ширину
+
+        self.food_button = Button(text='', background_normal='food.jpg')
+        self.food_text = Label(text='Блюдо', color=(0, 0, 0, 1))
+        self.sound_button = Button(text='', background_normal='sound.jpeg')
+        self.sound_text = Label(text='Случайный звук', color=(0, 0, 0, 1))
+        self.cube_button = Button(text='', background_normal='cube.jpg')
+        self.cube_text = Label(text='Бросить кубик', color=(0, 0, 0, 1))
         self.box.add_widget(self.main_text)
-        self.box.add_widget(self.image_food)
+        self.box.add_widget(self.food_button)
         self.box.add_widget(self.food_text)
-        self.box.add_widget(self.image_sound)
+        self.box.add_widget(self.sound_button)
         self.box.add_widget(self.sound_text)
-        self.box.add_widget(self.image_cube)
+        self.box.add_widget(self.cube_button)
         self.box.add_widget(self.cube_text)
         self.add_widget(self.box)
 
